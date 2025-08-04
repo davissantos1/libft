a05c7a542f5935321e4767174ee97662f34f0c03
Davi Santos <davisimoesdossantos1@gmail.com>
2025-08-04 09:47:24 -0300

    worked on gc

--- gc_malloc.c
+++ gc_malloc.c
@@ -1,35 +1,57 @@
 #include "libft.h"
 
-void	*gc_malloc_free_all(t_gc_malloc *head)
+void	*gc_create_node(t_gc *gc, size_t size, t_gc_tag tag)
 {
-	t_gc_malloc *tmp;
-
-	while (head)
+	t_gc_node	*head;
+	t_gc_node	*node;
+	
+	head = gc->lists[tag];
+	if (!head)
 	{
-		tmp = head;
-		if (head->ptr && head->tag)
-			free(head->ptr);
+			head  = malloc(sizeof(t_gc_node));
+			if (!head)
+				return (NULL);
+			head->ptr = malloc(size);
+			if (!head->ptr)
+				return (NULL);
+			head->next = NULL;
+			gc->lists[tag] = head;
+			return (head->ptr);
+	}
+	node = malloc(sizeof(t_gc_node));
+	if (!node)
+		return (NULL);
+	node->ptr = malloc(size);
+	if (!node->ptr)
+		return (NULL);
+	node->next = NULL;
+	ft_lstadd_back(head, node);
+	return (node->ptr);
+}
+
+void	*gc_free_tag(t_gc_malloc *head)
+{
+
+}
+void	*gc_free_all(t_gc_malloc *head)
+{
 		
 		
+}
 	
 void	*gc_malloc(size_t size, t_gc_tag tag)
 {
 	static t_gc *gc;
-	t_gc_node	*head;
+	t_gc_node	*node;
 
-	head = gc->lists[tag];
 	if (!gc)
 	{
 		gc = malloc(sizeof(t_gc));
 		if (!gc)
 			return (NULL);
 	}
-	if (!head)
-	{
-			head  = malloc(sizeof(t_gc_node));
-			if (!head)
-				return (NULL);
-	}
+	node = gc_create_node(gc, size, tag);
+
 	
 }
 	
